597c64084f84eef573b4f407bfa36711c029acb5
sybroo <74767501+sybroo@users.noreply.github.com>
2023-09-29 10:40:29 +0000

    de grafiek wordt geplot en je kan de herhalingen en het aantal trainen veranderen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from bke import MLAgent, is_winner, opponent, load, validate, RandomAgent, plot_validation
- 
+import random
+from bke import MLAgent, is_winner, opponent, RandomAgent, train_and_plot
  
 class MyAgent(MLAgent):
     def evaluate(self, board):
@@ -10,13 +10,17 @@ class MyAgent(MLAgent):
         else:
             reward = 0
         return reward
-    
  
-my_agent = load('MyAgent_3000')
-my_agent.learning = False
+random.seed(1) #seed bepaald het begin getal van de gegevens die je terug krijgt
  
-validation_agent = RandomAgent()
+my_agent = MyAgent()
+random_agent = RandomAgent()
  
-validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
- 
-plot_validation(validation_result)
+train_and_plot(
+    agent=my_agent,
+    validation_agent=random_agent,
+    iterations=50,
+    trainings=100,
+    validations=1000)
+
+#door deze code ontstaat een grafiek die laat zien hoevaak je hem traint en hoe vaak je hem valideert, iterations is hoe vaak die herhaald word
