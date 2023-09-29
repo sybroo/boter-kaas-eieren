5c9006d094f4a329df89a1f5c7704e16367807ed
sybroo <74767501+sybroo@users.noreply.github.com>
2023-09-29 11:18:58 +0000

    de hele grafiek met uitleg, eindproduct

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ class MyAgent(MLAgent):
  
 random.seed(1)
  
-my_agent = MyAgent(alpha=0.1, epsilon=0.1)
+my_agent = MyAgent(alpha=0.1, epsilon=0.05) 
+
+#hier wordt bepaald hoe snel de agent nieuwe kennis adopteert hoe hoger dit getal hoe sneller de agent geneigd zal zijn om oude kennis te vervangen door nieuwe kennis
+
 random_agent = RandomAgent()
  
 train_and_plot(
@@ -22,9 +25,12 @@ train_and_plot(
     iterations=100,
     trainings=100,
     validations=1000)
+#hier wordt my_agent getraind tegen de random agent met 100 herhalingen en 100 trainingen en daarbij worden de gegevens 1000 keer gechecked. 
 
 validation_agent = RandomAgent()
  
 validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=100)
  
 plot_validation(validation_result)
+
+#hier wordt de getrainde my_agent tegen een nieuwe random agent gezet en spelen ze 100 potjes daarvan wordt de grafiek gegeven hoeveel er gewonnen worden
